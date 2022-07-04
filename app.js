7297e93b0592f23ed92eda000f372dcf7fa1a14c
Jordan96L <jordan-ling@hotmail.co.uk>
2022-07-04 15:40:18 +0100

    add get for review id and also add error handling and tests

--- app.js
+++ app.js
@@ -6,5 +6,15 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/categories", controllers.getCategories);
+app.get("/api/reviews/:review_id", controllers.getReviewById);
 
+app.use("*", (req, res) => {
+  res.status(404).send({ msg: "Invalid Path" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
 module.exports = app;
