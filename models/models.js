b97a22b2aacd77d8e82758c84967c73747a847ca
Jordan96L <jordan-ling@hotmail.co.uk>
2022-07-07 14:21:51 +0100

    add endpoints with tests for delete comments by id

--- models/models.js
+++ models/models.js
@@ -196,3 +196,23 @@ exports.checkCategoryExists = (category) => {
     }
   });
 };
+
+exports.removeCommentById = (comment_id) => {
+  return db
+    .query(
+      `
+  DELETE FROM comments
+  WHERE comment_id = $1
+  RETURNING comment_id AS deleted
+  `,
+      [comment_id]
+    )
+    .then(({ rows, rowCount }) => {
+      if (rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Comment ID does not exist",
+        });
+      } else return rows[0];
+    });
+};
