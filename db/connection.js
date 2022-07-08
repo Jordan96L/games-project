13e4c902f92b03f3e58936ff2eddc3495cc00b64
Jordan96L <jordan-ling@hotmail.co.uk>
2022-07-08 11:21:07 +0100

    remove comments

--- db/connection.js
+++ db/connection.js
@@ -12,13 +12,6 @@ const config =
         },
       }
     : {};
-<<<<<<< HEAD
-console.log(process.env.DATABASE_URL);
-// if (!process.env.PGDATABASE) {
-//   throw new Error("PGDATABASE not set");
-// }
-=======
->>>>>>> 2165965af2e34b2b44d3bf1b139dc6a26f383a7c
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
