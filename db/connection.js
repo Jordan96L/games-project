60ab03ad5acf357942b53238192fd65f769e8d3f
Jordan96L <jordan-ling@hotmail.co.uk>
2023-01-16 13:43:18 +0000

    add changes for new deployment/hosting

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
-  path: `${__dirname}/../${ENV}.env`,
+  path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
@@ -14,8 +14,8 @@ const config =
   ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        max: 2,
       }
     : {};
+
+module.exports = new Pool(config);
